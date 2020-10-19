c7a3f774ad318982077f5dd4292c613d4321ac5f
unknown <mitumariam18@gmail.com>
2020-10-19 00:32:57 -0700

    Navbar context API setup

--- src/context/context.js
+++ src/context/context.js
@@ -6,9 +6,42 @@ const ProductContext = React.createContext();
 //provider
 //consumer
 class ProductProvider extends Component {
+state = {
+    sidebarOpen: false,
+    cartOpen: false,
+    cartItems: 0
+}
+
+//handle sidebar
+handleSidebar = () => {
+    this.setState({ sidebarOpen: !this.state.sidebarOpen }) 
+}
+//handle Cart
+handleCart = () => {
+    this.setState({ cartOpen: !this.state.cartOpen }) 
+}
+
+//close cart
+closeCart = () => {
+    this.setState({ cartOpen: false }) 
+
+}
+
+//open cart
+openCart = () => {
+    this.setState({ cartOpen: true }) 
+
+}
+
   render() {
     return (
-      <ProductContext.Provider value="hello from context">
+      <ProductContext.Provider value={{
+          ...this.state,
+          handleSidebar: this.handleSidebar,
+          handleCart: this.handleCart,
+          closeCart: this.closeCart,
+          openCart: this.openCart
+      }}>
         {this.props.children}
       </ProductContext.Provider>
     );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,9 @@ import { ProductConsumer } from "../context";
 export default function HomePage() {
     return (
         <ProductConsumer>
-           {value => <h1>{value}</h1> }
+           {value => {console.log({value})
+           return <h2>home pg</h2>
+           }}
         </ProductConsumer>
     )
 }
